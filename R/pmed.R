3cea517820988daacead18a1bdabd0067ad92647
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2021-09-23 12:13:54 +0200

    Update pmed.R

--- R/pmed.R
+++ R/pmed.R
@@ -17,14 +17,6 @@ pmed=function(x,mu=0,Sigma=1,rho=0,nu=1){
     stop("nu must be a real.")
   }
   
-  if(length(x)>1){
-    stop("x must be a real.")
-  }
-  
-  #if(is.matrix(Sigma)!=F){
-  #  stop("Sigma must be a matrix.")
-  #}
-  
   if(length(Sigma)>1){
     stop("Sigma must be a real.")
   }
