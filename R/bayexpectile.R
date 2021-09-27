0e4d9639af49d0746b18ffd11e88f0f446268351
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2021-09-27 15:16:22 +0200

    Add files via upload

---
+++ R/bayexpectile.R
@@ -0,0 +1,32 @@
+bayexpectile=function(X,dir=c(1,rep(0,length(X[1,])-1)),rho=0.5,niter=100){
+
+  if(rho>=1 || rho<0){
+    stop("rho must be between 0 and 1.")
+  }
+
+  if(sum(dir^2)!=1){
+    stop("dir must be on the unit circle.")
+  }
+
+  nu_tilde=dir
+
+  n=length(X[,1])
+
+  d=length(X[1,])
+
+  med_dat <- list(n = n,
+                  d = d,
+                  rho=rho,
+                  nu_tilde=nu_tilde,
+                  Y = X
+  )
+
+  fit <- stan(file = 'expectile.stan', data = med_dat,
+              chains = 2, iter = niter)
+
+  return(list(mu=extract(fit)$mu,muhat=apply(extract(fit)$mu,2,mean)))
+
+}
+
+
+
