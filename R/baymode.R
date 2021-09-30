1103d15e710f40b623290ec4c1b60264ef6cc8fc
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2021-09-30 15:52:02 +0200

    Update baymode.R

--- R/baymode.R
+++ R/baymode.R
@@ -9,7 +9,7 @@ baymode=function(X,niter=100){
                   Y = X
   )
 
-  fit <- stan(file = 'mode.stan', data = med_dat,
+  fit <- sampling(object = stanmodels$mode, data = med_dat,
               chains = 2, iter = niter)
 
   return(list(mu=extract(fit)$mu,muhat=apply(extract(fit)$mu,2,mean)))
